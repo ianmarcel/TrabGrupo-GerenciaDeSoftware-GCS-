c5da7b2f8e42e92bac6c7681be72e9f19af4945d
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-03-30 12:29:39 -0300

    Setup dos testes

---
+++ src/test/java/br/travelexpense/model/ClienteTest.java
@@ -0,0 +1,64 @@
+package br.travelexpense.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ClienteTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetEndereco() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEndereco() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetViagens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetViagens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCnpj() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCnpj() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNome() {
+		fail("Not yet implemented");
+	}
+
+}
