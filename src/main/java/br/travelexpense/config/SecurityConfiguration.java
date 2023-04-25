6cd426e10399e284a8f01629a703283d2f3bdba9
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-04-25 20:16:19 -0300

    Correção do security config

--- src/main/java/br/travelexpense/config/SecurityConfiguration.java
+++ src/main/java/br/travelexpense/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ import br.travelexpense.utils.CookieHelper;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-	private static final long VADLID_TOKEN_TIME = 604800L; //7 dias
+	private static final int VADLID_TOKEN_TIME = 604800; // 7 dias
 
 	private final UserDetailsServiceImpl userDetailsService;
 
@@ -28,21 +28,22 @@ public class SecurityConfiguration {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-			//.and().authorizeHttpRequests().anyRequest().permitAll()
-			//.httpBasic()
-			.and()
-			.authorizeHttpRequests()
-				//.antMatchers("/auth/**").permitAll()
-			//.antMatchers("/empresa*/**").hasAnyRole("GESTOR", "DONO")
-			.anyRequest().permitAll()
-			.and()
-			.cors().disable()
-			.csrf().disable()
-			.formLogin().disable()
-			.rememberMe().alwaysRemember(true).key("session_key").tokenValiditySeconds(VADLID_TOKEN_TIME).rememberMeCookieName("LOGADO")
-			.and()
-			.logout().deleteCookies("JSESSIONID", CookieHelper.ID_EMPRESA_COOKIE)
-			.and();
+				// .and().authorizeHttpRequests().anyRequest().permitAll()
+				// .httpBasic()
+				.and()
+				.authorizeHttpRequests()
+				// .antMatchers("/auth/**").permitAll()
+				// .antMatchers("/empresa*/**").hasAnyRole("GESTOR", "DONO")
+				.anyRequest().permitAll()
+				.and()
+				.cors().disable()
+				.csrf().disable()
+				.formLogin().disable()
+				.rememberMe().alwaysRemember(true).key("session_key").tokenValiditySeconds(VADLID_TOKEN_TIME)
+				.rememberMeCookieName("LOGADO")
+				.and()
+				.logout().deleteCookies("JSESSIONID", CookieHelper.ID_EMPRESA_COOKIE)
+				.and();
 		return http.build();
 	}
 
@@ -58,6 +59,5 @@ public class SecurityConfiguration {
 				.passwordEncoder(passwordEncoder())
 				.and().build();
 	}
-
 
 }
